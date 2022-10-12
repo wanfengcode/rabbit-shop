2ad62c1591622c77cc97356b090a89f097ab762c
wanfeng <2801407337@qq.com>
2022-10-12 09:17:32 +0800

    批量注册组件 一级类目页面基础布局

--- src/components/libs/index.js
+++ src/components/libs/index.js
@@ -7,11 +7,19 @@
         3.通过app的component方法将插件名和插件进行配置
     使用时，在main.js中导入，通过use方法将插件进行注册
 */
-import RabbitCarousel from './RabbitCarousel.vue'
-import RabbitMore from './RabbitMore.vue'
-import RabbitBread from './RabbitBread.vue'
-import RabbitBreadItem from './RabbitBreadItem.vue'
+// import RabbitCarousel from './RabbitCarousel.vue'
+// import RabbitMore from './RabbitMore.vue'
+// import RabbitBread from './RabbitBread.vue'
+// import RabbitBreadItem from './RabbitBreadItem.vue'
 import defaultImg from '@/assets/images/200.png'
+/*
+context(dir,deep,match)用于加载某一目录下的文件
+dir为需要加载的文件目录
+deep为是否需要加载子目录
+match为正则表达式，用于匹配目标文件
+context()返回一个函数用于导入文件，该函数具备一个keys()方法获取匹配后的所有文件
+*/
+const importFn = require.context('./', false, /\.vue$/)
 
 const defineDirective = (app) => {
   // 图片懒加载指令
@@ -36,10 +44,18 @@ const defineDirective = (app) => {
 export default {
   install (app) {
     // 将封装的插件注册到整个项目的根实例中,即全局注册
-    app.component(RabbitCarousel.name, RabbitCarousel)
-    app.component(RabbitMore.name, RabbitMore)
-    app.component(RabbitBread.name, RabbitBread)
-    app.component(RabbitBreadItem.name, RabbitBreadItem)
+    // app.component(RabbitCarousel.name, RabbitCarousel)
+    // app.component(RabbitMore.name, RabbitMore)
+    // app.component(RabbitBread.name, RabbitBread)
+    // app.component(RabbitBreadItem.name, RabbitBreadItem)
+
+    // 遍历匹配到的所有组件
+    importFn.keys().forEach(item => {
+      // 导入组件，default表明导入组件的默认导出也就是export default
+      const component = importFn(item).default
+      // 注册组件
+      app.component(component.name, component)
+    })
 
     // 将自定义的指令注册到根实例中
     defineDirective(app)
--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -1,25 +1,136 @@
 <template>
   <div class="topCategory">
     <div class="container">
-    <!-- 面包屑导航 -->
-    <RabbitBread>
-      <rabbit-bread-item to='/'>首页</rabbit-bread-item>
-      <rabbit-bread-item to='/category/1005001'>电器</rabbit-bread-item>
-      <rabbit-bread-item >空调</rabbit-bread-item>
-    </RabbitBread>
-    <!-- 轮播图 -->
-    <!-- 全部分类 -->
-    <!-- 分类下的具体商品 -->
-  </div>
+      <!-- 面包屑导航 -->
+      <RabbitBread>
+        <rabbit-bread-item to="/">首页</rabbit-bread-item>
+        <rabbit-bread-item>{{ topCategory.name }}</rabbit-bread-item>
+      </RabbitBread>
+      <!-- 轮播图 -->
+      <rabbit-carousel
+        autoPlay
+        :list="bannerList"
+        style="height: 500px"
+      ></rabbit-carousel>
+      <!-- 一级分类下的二级分类 -->
+      <div class="subList">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="item in topCategory.children" :key="item.id">
+            <a href="javascript:;">
+              <img :src="item.picture" />
+              <p>{{ item.name }}</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- 二级分类下的具体商品 -->
+      <div class="ref-goods">
+        <div class="head">
+          <h3>- 海鲜 -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <RabbitMore class="rabbit-more"></RabbitMore>
+        </div>
+        <div class="body">
+          <CategoryGoodsItem v-for="i in 5" :key="i"></CategoryGoodsItem>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { computed, ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
+import { findBanner } from '@/api/home'
+import CategoryGoodsItem from './components/CategoryGoodsItem'
 export default {
-  name: 'TopCatagory'
+  name: 'TopCatagory',
+  components: { CategoryGoodsItem },
+  setup () {
+    // 获取轮播图数据
+    const bannerList = ref([])
+    findBanner().then((data) => {
+      bannerList.value = data.result
+    })
+
+    // 获取全部分类
+    const store = useStore()
+    const route = useRoute()
+    const topCategory = computed(() => {
+      const item = store.state.category.list.find((item) => {
+        return item.id === route.params.id
+      })
+      return item
+    })
+
+    return { bannerList, topCategory }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.topCategory {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .subList {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @xtxColor;
+          }
+        }
+      }
+    }
+  }
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+    .head {
+      .rabbit-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
+    }
+  }
+}
 </style>
