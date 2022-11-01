21b9c3080bb1a3b5a8447682bcd0ceacf5d86658
wanfeng <2801407337@qq.com>
2022-11-01 14:49:39 +0800

    登录模块-表单校验

---
+++ src/utils/vee-validation-schema.js
@@ -0,0 +1,33 @@
+
+export default {
+  // 用户名校验
+  account (value) {
+    if (!value) return '请输入用户名'
+    // 校验规则：字母开头，6~20个字符
+    if (!/^[a-zA-Z]\w{5,19}$/.test(value)) return '字母开头6~20个字符'
+    return true
+  },
+  password (value) {
+    if (!value) return '请输入密码'
+    // 6~24个字符
+    if (!/^\w{6,}$/.test(value)) return '6~24个字符'
+    return true
+  },
+  mobile (value) {
+    if (!value) return '请输入手机号'
+    // 1开头 3~9 9个数字结尾
+    if (!/^1[3-9]\d{9}$/.test(value)) return '号码格式不对'
+    return true
+  },
+  code (value) {
+    if (!value) return '请输入验证码'
+    // 6位数字
+    if (!/^\d{6}$/.value) return '验证码为六位数字'
+    return true
+  },
+  isAgree (value) {
+    if (!value) return '请勾选用户协议'
+    return true
+  }
+
+}
--- src/views/login/components/LoginForm.vue
+++ src/views/login/components/LoginForm.vue
@@ -2,53 +2,58 @@
   <div class="accountBox">
     <div class="toggle">
       <a @click="isMsgLogin=false" href="javascript:;" v-if="isMsgLogin">
-        <i class="iconfont icon-user"></i> 使用账号登录
+        <i class="iconfont icon-user"></i> 使用短信登录
       </a>
       <a @click="isMsgLogin=true" href="javascript:;" v-else>
-        <i class="iconfont icon-msg"></i> 使用短信登录
+        <i class="iconfont icon-msg"></i> 使用账号登录
       </a>
     </div>
-    <div class="form">
+    <Form ref="formTarget" class="form" :validation-schema="localSchema" v-slot="{errors}">
       <template v-if="isMsgLogin">
         <div class="form-item">
           <div class="input">
             <i class="iconfont icon-user"></i>
-            <input type="text" placeholder="请输入用户名" />
+            <Field :class="{error:errors.account}" v-model="form.account" name='account' type="text" placeholder="请输入用户名"></Field>
           </div>
+          <div class="error" v-if="errors.account"><i class="iconfont icon-warning" />{{errors.account}}</div>
         </div>
         <div class="form-item">
           <div class="input">
             <i class="iconfont icon-lock"></i>
-            <input type="password" placeholder="请输入密码">
+            <Field :class="{error:errors.password}" v-model="form.password" name="password" type="password" placeholder="请输入密码"></Field>
           </div>
+          <div class="error" v-if="errors.password"><i class="iconfont icon-warning" />{{errors.password}}</div>
         </div>
       </template>
       <template v-else>
         <div class="form-item">
           <div class="input">
             <i class="iconfont icon-user"></i>
-            <input type="text" placeholder="请输入手机号" />
+            <Field :class="{error:errors.mobile}" v-model="form.mobile" name="mobile" type="text" placeholder="请输入手机号"></Field>
           </div>
+          <div class="error" v-if="errors.mobile"><i class="iconfont icon-warning" />{{errors.mobile}}</div>
         </div>
         <div class="form-item">
           <div class="input">
             <i class="iconfont icon-code"></i>
-            <input type="password" placeholder="请输入验证码">
+            <Field :class="{error:errors.code}" v-model="form.code" name="code" type="password" placeholder="请输入验证码"></Field>
             <span class="code">发送验证码</span>
           </div>
+          <div class="error" v-if="errors.code"><i class="iconfont icon-warning" />{{errors.code}}</div>
         </div>
       </template>
       <div class="form-item">
         <div class="agree">
-          <RabbitCheckedBox v-model="form.isAgree"></RabbitCheckedBox>
+          <Field as="RabbitCheckedBox" name="isAgree" v-model="form.isAgree"></Field>
           <span>我已同意</span>
           <a href="javascript:;">《隐私条款》</a>
           <span>和</span>
           <a href="javascript:;">《服务条款》</a>
         </div>
+        <div class="error" v-if="errors.isAgree"><i class="iconfont icon-warning" />{{errors.isAgree}}</div>
       </div>
-      <a href="javascript:;" class="btn">登录</a>
-    </div>
+      <a @click="login" href="javascript:;" class="btn">登录</a>
+    </Form>
     <div class="action">
       <img src="https://qzonestyle.gtimg.cn/qzone/vas/opensns/res/img/Connect_logo_7.png" alt="">
       <div class="url">
@@ -60,18 +65,58 @@
 </template>
 
 <script>
-import { reactive, ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
+import { Form, Field } from 'vee-validate'
+import schema from '@/utils/vee-validation-schema'
 export default {
   name: 'LoginForm',
+  components: {
+    Form,
+    Field
+  },
   setup () {
     // 切换登录方式
     const isMsgLogin = ref(true)
     // 表单校验信息
     const form = reactive({
-      isAgree: false
+      isAgree: true,
+      account: null,
+      password: null,
+      mobile: null,
+      code: null
+    })
+    const formTarget = ref(null)
+    /*
+        vee-validate插件使用步骤
+        1.按需导入该插件的Form、Field组件;分别用于替换表单标签和input标签
+        2.Form组件提供 validation-schema 属性用于接收校验规则对象,添加v-slot="{errors}"作用域插槽用于提供错误对象errors
+            2.1 Form组件提供validate函数用于表单的整体校验，返回结果为promise对象
+        3.Field组件添加name属性，取值应和校验规则对象里的方法同名用于触发同名函数完成对input框值变化时的校验；双向数据绑定相应input框的值
+            3.1 Field的as属性支持将当前Field组件转为其他标签或组件，但组件必须支持v-model否则校验不触发
+    */
+    // 校验规则对象
+    const localSchema = {
+      isAgree: schema.isAgree,
+      account: schema.account,
+      password: schema.password,
+      mobile: schema.mobile,
+      code: schema.code
+    }
+    // 登录按钮触发事件
+    const login = async () => {
+      const validResult = await formTarget.value.validate()
+      console.log(validResult)
+    }
+    // 切换登陆方式后，清空表单信息
+    watch(isMsgLogin, () => {
+      form.isAgree = true
+      form.account = null
+      form.password = null
+      form.mobile = null
+      form.code = null
     })
 
-    return { isMsgLogin, form }
+    return { isMsgLogin, form, localSchema, login, formTarget }
   }
 }
 </script>
